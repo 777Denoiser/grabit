d4a4c74c9c266ec99330107a8044ed1065502fe7
Muhammad Uzair Seyal <37130007+777Denoiser@users.noreply.github.com>
2024-12-04 01:22:32 -0500

    Update resource.go
    deleted my code under the geturltodir function and added the missing line that was requested

--- internal/resource.go
+++ internal/resource.go
@@ -69,20 +69,7 @@ func GetUrlToDir(u string, targetDir string, ctx context.Context) (string, error
 	h := sha256.New()
 	h.Write([]byte(u))
 	fileName := filepath.Join(targetDir, fmt.Sprintf(".%s", hex.EncodeToString(h.Sum(nil))))
-
-	// Create new file with immediate close
-	file, err := os.Create(fileName)
-	if err != nil {
-		return "", fmt.Errorf("failed to create file: %v", err)
-	}
-	file.Close()
-
-	fileName, err = getUrl(u, fileName, ctx)
-	if err != nil {
-		return "", err
-	}
-
-	return fileName, nil
+		return getUrl(u, filename, ctx)
 }
 
 // GetUrlWithDir downloads the given resource to a temporary file and returns the path to it.
